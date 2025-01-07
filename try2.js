0700c317fe38d9a1e7c7d180317ad09caa43dd68
Ayush Batham <117055541+ayush8627@users.noreply.github.com>
2025-01-07 23:12:30 +0530

    Update try2.js

--- try2.js
+++ try2.js
@@ -1,3 +1,12 @@
+window.onload = function () {
+  setTimeout(function () {
+    // Hide the loading screen
+    document.getElementById("loading").style.display = "none";
+
+    // Show the main content
+    document.getElementById("content").style.display = "block";
+  }, 5000); // 5000ms = 5 seconds
+};
 // Retrieve the username from localStorage
 const username = localStorage.getItem('username');
 
